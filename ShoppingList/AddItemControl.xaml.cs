6cffeb2613224a35b16685fcde8c97715b484114
SameenMahdoubi <51130772+SameenMahdoubi@users.noreply.github.com>
2020-02-24 17:06:20 -0600

    Uploaded files from in class

---
+++ ShoppingList/AddItemControl.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShoppingList
+{
+    /// <summary>
+    /// Interaction logic for AddItemControl.xaml
+    /// </summary>
+    public partial class AddItemControl : UserControl
+    {
+        public AddItemControl()
+        {
+            InitializeComponent();
+            AddItemButton.Click += OnAddItemButtonClick;
+        }
+
+        protected void OnAddItemButtonClick(object sender, RoutedEventArgs e)
+        {   /*
+            var item = ItemToAdd.Text;
+            var data = (ShoppingListData)DataContext;
+            var data2 = DataContext as ShoppingListData;
+            if (data2 != null)
+            {
+                // safe to work with data2
+            }
+            */
+            if (DataContext is ShoppingListData data3)
+            {
+                // makes data3 if the casting is successful, local variable for that block of code
+                data3.Add(ItemToAdd.Text);
+                ItemToAdd.Text = "";
+            }
+        }
+    }
+}
---
+++ ShoppingList/ShoppingListData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace ShoppingList
+{
+    public class ShoppingListData : INotifyPropertyChanged
+    {
+        List<string> items = new List<string>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public IEnumerable<string> Items => items.ToArray(); // this property is a copy of items, rather than a regular property (this preserves data hiding)
+
+        public void Add(string item)
+        {
+            items.Add(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+
+        }
+
+        public void Remove(string item)
+        {
+            items.Remove(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+        }
+    }
+}
